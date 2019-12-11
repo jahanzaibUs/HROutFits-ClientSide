5f9d6d463fe298e53b8eafc135b8516452216380
root <zaibsmiu@gmail.com>
2019-12-11 10:56:44 +0500

    Set Mens pages

---
+++ src/component/Men/denim/denim.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBContainer, MDBRow } from 'mdbreact';
+import Shirt from "../../assets/Mens/shirts.jpg"
+class DenimComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            Items: [
+                    {Title: "Red Hoodie", Price: "1200", img: {Shirt}, },
+                    {Title: "Red Hoodie", Price: "1200", img: {Shirt}, }
+                ]
+        }
+    }
+
+    render(){
+
+        return (
+            <MDBContainer className="mt-5">
+        <h1>Denim Arrivals</h1>
+      <MDBRow>
+          {
+              this.state.Items.map((text,index) =>{
+                  console.log(text.img)
+                  return(
+                        <MDBCol md="4" key={index}>
+                            <Link to="/cart">
+                            <MDBCard style={{ width: "20rem" }}>
+                                <MDBCardImage className="img-fluid" src={text.img.Shirt}
+                                // "https://mdbootstrap.com/img/Photos/Others/images/43.jpg"
+                                waves />
+                                <MDBCardBody>
+                                <MDBCardTitle>{text.Title}</MDBCardTitle>
+                                <MDBCardText className="FontColorDefault FontSize22px FontWeightBold">
+                                Rs. {text.Price}
+                                </MDBCardText>
+                                <MDBBtn href="#" className="Width100" >Add to Cart</MDBBtn>
+                                </MDBCardBody>
+                            </MDBCard>
+                            </Link>
+                        </MDBCol>
+                  )
+              })
+          }
+           </MDBRow>
+        </MDBContainer>
+        )   
+    }
+}
+export default DenimComponent;
